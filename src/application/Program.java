e8c8870725840ffe10929e5e3899275bd9912c87
Valter Lafuente Junior <valterlafuente@yahoo.com.br>
2020-02-14 18:56:19 -0300

    Projeto iniciado

---
+++ src/application/Program.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    
+    public static void main(String[] args) {
+       
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+    }
+    
+}
---
+++ src/entities/Circle.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+public class Circle extends Shape{
+    
+    private Double radius;
+    
+    public Circle(){
+        super();
+    }
+
+    public Circle(Double radius, entities.enums.Color color) {
+        super(color);
+        this.radius = radius;
+    }
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+       return Math.PI * radius * radius;
+    }
+    
+    
+}
